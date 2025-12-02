a2f50b988a7c41f6b8263e819fb3802f9694660f
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2025-12-02 20:53:00 +0700

    Коммит

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,3 @@
-function getMinutes(timeStr) {
-  const [hours, minutes] = timeStr.split(':').map(Number);
-  return hours * 60 + minutes;
-}
 function isMeetingInWorkHours(startDay, endDay, startMeeting, duration) {
   const startDayMin = getMinutes(startDay);
   const endDayMin = getMinutes(endDay);
@@ -9,4 +5,8 @@ function isMeetingInWorkHours(startDay, endDay, startMeeting, duration) {
   const endMeetingMin = startMeetingMin + duration;
 
   return startMeetingMin >= startDayMin && endMeetingMin <= endDayMin;
+}
+function getMinutes(timeStr) {
+  const [hours, minutes] = timeStr.split(':').map(Number);
+  return hours * 60 + minutes;
 }
